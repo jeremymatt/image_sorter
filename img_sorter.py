03ebda1a02fb9bd411ab43f81ec2f5da8ae426f3
Jeremy Matt <jeremy.matt@gmail.com>
2021-11-02 23:24:28 -0400

    Working Checkpoint: zoom approx. to mouse location working

--- img_sorter.py
+++ img_sorter.py
@@ -46,7 +46,6 @@ class App:
         
         self.source_dir = settings.source_dir
         
-        
         self.get_img_list()
         
         self.previous_images = []
@@ -87,7 +86,6 @@ class App:
         self.set_keybindings(self.parent)
         
         self.init_first_image()
-        # self.show_nofiles_window()
         
     def reset_zoomcycle(self):
         self.zoomcycle = 0
@@ -111,14 +109,9 @@ class App:
                 dirs_to_process.extend([os.path.join(cur_dir,item) for item in new_dirs])
             
             
-            
-        # if len(self.img_list) == 0:
-        #     self.show_nofiles_window()
         
     def show_menu_window(self,dummy=None):
         self.menu_window = tk.Toplevel(self.parent)
-        # self.menu_window.attributes('-fullscreen', True)
-        # self.menu_window.configure(background='black')
         canvas = tk.Canvas(self.menu_window,height=500,width=500)
         canvas.pack()
         menu_txt = self.gen_menutext()
@@ -134,42 +127,6 @@ class App:
         canvas.move(text_item,int(dim[0]/4),int(dim[1]/3))
         canvas.update()
         canvas.tag_raise(text_item)
-        
-    def show_nofiles_window(self,dummy=None):
-        self.nofiles_window = tk.Toplevel(self.parent)
-        self.nofiles_window.focus_force()
-        
-        
-        self.nofiles_window.bind("<Control-q>",self.quit_app)
-        self.nofiles_window.bind("<Escape>",self.quit_app)
-        self.nofiles_window.bind("<Control-z>",self.undo)
-        
-        # self.window.attributes('-fullscreen', True)
-        # self.window.configure(background='black')
-        canvas = tk.Canvas(self.nofiles_window,height=50,width=100)
-        canvas.pack()
-        error_text = 'No images remain in list'
-        text_item = canvas.create_text(
-            int(self.img_window_width/2),
-            int(self.img_window_height/2),
-            fill='lightblue',
-            font='times 10 bold',
-            text=error_text,
-            tag='error_txt')
-        canvas.tag_raise(text_item)
-        # text_item = canvas.create_text(
-        #     int(100),
-        #     int(100),
-        #     fill='black',
-        #     font='times 10 bold',
-        #     text=error_text,tag='error_txt')
-        bbox = canvas.bbox(text_item)
-        dim = (bbox[2]-bbox[0]+50,bbox[3]-bbox[1]+50)
-        self.nofiles_window.geometry(f'{dim[0]}x{dim[1]}+0+0')
-        canvas.move(text_item,int(dim[0]/4),int(dim[1]/3))
-        canvas.update()
-        canvas.tag_raise(text_item)
-        
         
     def set_keybindings(self,window):
         window.bind('<F11>', self.toggle_fs)
@@ -190,14 +147,6 @@ class App:
         window.bind('<Alt-m>',self.toggle_menu)
         window.bind('<KeyRelease>',self.keyup)
         
-        
-    # def tr(rotation):
-        
-    #     sign = rotation/abs(rotation)
-    #     rotation %= 360
-    #     rotation *= sign
-    #     rotation  = int(rotation)
-    #     print(rotation)
         
     def rotate_cw(self,dummy=None):
         self.rotation += 90
@@ -232,7 +181,6 @@ class App:
         self.init_image()
         
         
-        
     def toggle_rand_order(self,dummy=None):
         if self.rand_order:
             self.rand_order = False
@@ -243,7 +191,6 @@ class App:
     
     def motion(self,event):
         self.mouse_x,self.mouse_y = event.x,event.y
-        # print("motion: {}, {}".format(self.mouse_x,self.mouse_y))
         
     def move_from(self,event):
         self.start_x = event.x
@@ -304,14 +251,12 @@ class App:
         
         self.move_events.append((file,dest_file,cp.deepcopy(self.cur_img),cp.deepcopy(self.img_list)))
         
-        
         move(file,dest_file)
         
         self.img_list.remove(file)
         if self.cur_img >= len(self.img_list):
             self.cur_img = 0
             
-        
         self.init_image()
         
         
@@ -324,14 +269,10 @@ class App:
         self.init_image()
         
     def zoomer(self,event):
-        # (x,y) = self.to_raw((event.x,event.y))
-
         if (event.delta > 0 and self.zoomcycle < self.MAX_ZOOM):
             self.zoomcycle += 1
-            # print(f'zoomcycle: {self.zoomcycle}, scale: {self.mux[self.zoomcycle]}')
         elif (event.delta < 0 and self.zoomcycle > self.MIN_ZOOM):
             self.zoomcycle -= 1
-            # print(f'zoomcycle: {self.zoomcycle}, scale: {self.mux[self.zoomcycle]}')
         else:
             print('Max/Min zoom reached!')
             return
@@ -385,7 +326,6 @@ class App:
         else:
             step = 1
         self.cur_img = (self.cur_img+step) % len(self.img_list)
-        # print(self.previous_images)
         self.new_image = True
         self.open_image.close()
         self.init_image()
@@ -460,102 +400,53 @@ class App:
         self.crop_bbox = [int(round(item)) for item in self.crop_bbox]
         
         
-        
-        
     def update_bbox_zoom(self):
-        print('bbox dim:{}x{}'.format(self.bbox_width,self.bbox_height))
-        print('old bbox: {}'.format(self.bbox))
-        print('old crop box: {} ({}x{})'.format(self.crop_bbox,self.crop_width,self.crop_height))
+        prev_img_edge = [(self.img_window_width-self.bbox_width)/2,(self.img_window_height-self.bbox_height)/2]
+        prev_img_wh = [self.new_img_width,self.new_img_height]
         self.new_img_width = int(self.img_width*self.ratio*self.mux[self.zoomcycle])
         self.new_img_height = int(self.img_height*self.ratio*self.mux[self.zoomcycle])
+        cur_img_wh = [self.new_img_width,self.new_img_height]
+        
         self.abs_ratio = self.ratio*self.mux[self.zoomcycle]
         
         self.bbox_width = min(self.new_img_width,self.img_window_width)
         self.bbox_height = min(self.new_img_height,self.img_window_height)
+        cur_bbox_wh = [self.bbox_width,self.bbox_height]
         
-        bb_wh_ratio = self.bbox_width/self.bbox_height
+        cur_mouse_xy = [self.mouse_x,self.mouse_y]
         
-        self.bbox_ratio_x = self.bbox_width/self.new_img_width
-        self.bbox_ratio_y = self.bbox_height/self.new_img_height
-        
-        
-        mouse_x_fraction = self.mouse_x/self.img_window_width
-        mouse_y_fraction = self.mouse_y/self.img_window_height
-        
-        self.bbox[0] = int(self.mouse_x-self.bbox_width*mouse_x_fraction)
-        self.bbox[2] = int(self.mouse_x+self.bbox_width*(1-mouse_x_fraction))
-        self.bbox[1] = int(self.mouse_y-self.bbox_height*mouse_y_fraction)
-        self.bbox[3] = int(self.mouse_y+self.bbox_height*(1-mouse_y_fraction))
-        
-        
-        
+        for i in range(2):
+            prev_mouse_pix = (cur_mouse_xy[i]-prev_img_edge[i]+self.bbox_anchor[i])
+            new_center = prev_mouse_pix*cur_img_wh[i]/prev_img_wh[i]
+            self.bbox_anchor[i] = new_center-cur_bbox_wh[i]/2
         
         self.bbox_ratio_x = (self.img_window_width/self.bbox_width)*self.mux[self.zoomcycle]
         self.bbox_ratio_y = (self.img_window_height/self.bbox_height)*self.mux[self.zoomcycle]
         
+        if self.bbox_anchor[0] < 0:
+            self.bbox_anchor[0] = 0
+            
+        if self.bbox_anchor[1] < 0:
+            self.bbox_anchor[1] = 0
+            
+        if self.bbox_anchor[0] + self.bbox_width > self.new_img_width:
+            self.bbox_anchor[0] = self.new_img_width - self.bbox_width
+            
+        if self.bbox_anchor[1] + self.bbox_height > self.new_img_height:
+            self.bbox_anchor[1] = self.new_img_height - self.bbox_height
+            
+        crop_bbox_anchor = [
+            self.bbox_anchor[0]*(self.img_width/self.new_img_width),
+            self.bbox_anchor[1]*self.img_height/self.new_img_height]
         
-        if (self.bbox_width == self.new_img_width) and (self.bbox_height == self.new_img_height):
-            self.crop_bbox[0] = 0
-            self.crop_bbox[2] = self.img_width
-            
-            self.crop_bbox[1] = 0
-            self.crop_bbox[3] = self.img_height
-            
-            
-        if (self.bbox_width == self.new_img_width) and (self.bbox_height < self.new_img_height):
-            self.crop_bbox[0] = 0
-            self.crop_bbox[2] = self.img_width
-            
-            self.crop_height = self.bbox_height/self.abs_ratio
-            print('intended crop height: {}'.format(self.crop_height))
-            
-            self.crop_bbox[1] = self.bbox[1]/self.abs_ratio
-            self.crop_bbox[3] = self.bbox[3]/self.abs_ratio
-            self.crop_height = self.crop_bbox[3] - self.crop_bbox[1]
-            print('actual crop height: {}'.format(self.crop_height))
-            
-            
-        if (self.bbox_width < self.new_img_width) and (self.bbox_height == self.new_img_height):
-            self.crop_bbox[1] = 0
-            self.crop_bbox[3] = self.img_height
-            
-            self.crop_width = self.bbox_width/self.abs_ratio
-            print('intended crop width: {}'.format(self.crop_width))
-            
-            self.crop_bbox[0] = self.bbox[0]/self.abs_ratio
-            self.crop_bbox[2] = self.bbox[2]/self.abs_ratio
-            self.crop_height = self.crop_bbox[3] - self.crop_bbox[1]
-            print('actual crop width: {}'.format(self.crop_width))
-            
-        if (self.bbox_width < self.new_img_width) and (self.bbox_height < self.new_img_height):
-            self.crop_bbox[0] = self.bbox[0]/self.abs_ratio
-            self.crop_bbox[2] = self.bbox[2]/self.abs_ratio
-            
-            self.crop_height = self.bbox_height/self.abs_ratio
-            print('intended crop height: {}'.format(self.crop_height))
-            
-            self.crop_bbox[1] = self.bbox[1]/self.abs_ratio
-            self.crop_bbox[3] = self.bbox[3]/self.abs_ratio
-            self.crop_height = self.crop_bbox[3] - self.crop_bbox[1]
-            print('actual crop height: {}'.format(self.crop_height))
-            
+        self.crop_height = self.bbox_height/self.abs_ratio
+        self.crop_width = self.bbox_width/self.abs_ratio
+        
+        self.crop_bbox[0:2] = crop_bbox_anchor
+        self.crop_bbox[2] = crop_bbox_anchor[0]+self.crop_width
+        self.crop_bbox[3] = crop_bbox_anchor[1]+self.crop_height
+        
         self.crop_bbox = [int(round(item)) for item in self.crop_bbox]
-            
-                        
-        self.crop_width = self.crop_bbox[2] - self.crop_bbox[0]
-        self.crop_height = self.crop_bbox[3] - self.crop_bbox[1]
-        
-        cb_wh_ratio = self.crop_width/self.crop_height
-        
-        print('img w/h ratios: \n   img: {}\n   crp: {}\n   bbx: {}\n   win: {}'.format(self.img_wh_ratio,cb_wh_ratio,bb_wh_ratio,self.img_window_wh_ratio))
-        
-        print('new bbox: {} ({}x{})'.format(self.bbox,self.bbox_width,self.bbox_height))
-        print('new crop box: {} ({}x{})'.format(self.crop_bbox,self.crop_width,self.crop_height))
-        
-        # self.img_window_wh_ratio
-        
-        
-            
             
         
     def resize_img(self,frames):
@@ -575,8 +466,6 @@ class App:
             self.abs_ratio = self.ratio*self.mux[self.zoomcycle]
             self.init_bbox()
         
-        
-            
         for frame in frames:
             thumbnail = frame.copy()
             
@@ -635,7 +524,6 @@ class App:
     def open_img_window(self,sequence):
         self.img_window = tk.Toplevel(self.parent)
         
-        # self.set_keybindings(self.img_window)
         self.img_window.bind("<MouseWheel>",self.zoomer)
         self.img_window.bind('<Motion>',self.motion)
         self.img_window.bind("<Control-q>",self.quit_app)
@@ -708,7 +596,7 @@ class App:
         else:
             r_flag = ''
             height = 5
-        counter_text = '{}({}/{} ({}){}'.format(fn,item,num_items,zoom_perc,r_flag)
+        counter_text = '{}({}/{} ({}:{}){}'.format(fn,item,num_items,self.zoomcycle,zoom_perc,r_flag)
         text_item = self.canvas.create_text(5,height,fill='lightblue',anchor='w',font='times 10 bold',text=counter_text,tag='ctr_txt')
         bbox = self.canvas.bbox(text_item)
         rect_item = self.canvas.create_rectangle(bbox,fill='black',tag='ctr_txt')
@@ -719,6 +607,6 @@ class App:
             
 
 root = tk.Tk()
-# root.attributes('-fullscreen', True)
+root.attributes('-fullscreen', True)
 app = App(root)
 root.mainloop()
