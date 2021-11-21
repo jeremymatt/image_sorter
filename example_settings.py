743bbfca251278183a3d24f6a911ece27a9ce5e1
Jeremy Matt <jeremy.matt@gmail.com>
2021-11-21 10:27:50 -0500

    example settings: updates and comments

--- example_settings.py
+++ example_settings.py
@@ -7,7 +7,9 @@ Created on Thu Oct 28 19:53:29 2021
 import os
 
 cd = os.getcwd()
-dest_root = os.path.join(cd,'sorted_pictures') #root directory to save moved images
+#root directory to save moved images. Does not need to exist, the program will
+#create it if necessary
+dest_root = os.path.join(cd,'sorted_pictures')
 
 source_dir = os.path.join(cd,'images') #Directory to open images from
 
@@ -18,12 +20,12 @@ include_sub_dirs = True
 #Display images in random order
 random_display_order = False
 
+#Defines the keystrokes used to move images.  For example, pressing period will
+#move the image to <dest_root>\trash\<image_filename>
+#If they don't exist, these directories will be created as-needed
 move_dict = {
     '.':'trash',
     's':'sunsets',
     'f':'fish',
-    'F':r'fish\cooked'
+    'F':r'fish\salmon'
     }
-
-for key in move_dict.keys():
-    move_dict[key] = os.path.join(dest_root,move_dict[key])
