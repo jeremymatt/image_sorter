7d14ce6ed95e788dbc24ed5a3f74c9f785a5577d
jeremymatt <jmatt@UVM.EDU>
2022-01-07 14:57:41 -0500

    Working copy and replace with empty placeholder files

--- copy_replace_files.py
+++ copy_replace_files.py
@@ -5,3 +5,59 @@ Created on Fri Jan  7 10:18:58 2022
 @author: jmatt
 """
 
+import settings
+import copy_replace_files_fns as CR
+
+import os
+
+from shutil import move
+
+
+
+source_dir_root = settings.copy_move_source_dir
+
+source_dir_name = os.path.split(source_dir_root)[1]
+root = os.path.split(source_dir_root)[0]
+
+dest_dir_root = os.path.join(root,'{}_new-files'.format(source_dir_name))
+
+if settings.to_copy == 'all':
+    dirs_to_copy = [item for item in os.listdir(source_dir_root) if os.path.isdir(os.path.join(source_dir_root,item))]
+else:
+    dirs_to_copy = settings.to_copy
+    
+dr = dirs_to_copy[0]
+for dr in dirs_to_copy:
+    sub_dirs = [item for item in os.listdir(os.path.join(source_dir_root,dr)) if (item not in settings.dirs_to_ignore) and (os.path.isdir(os.path.join(source_dir_root,dr,item)))]
+    sdr = sub_dirs[0]
+    files_list = []
+    known_list_fn = os.path.join(source_dir_root,dr,settings.store_table_locn,'.copied_files')
+    if os.path.isfile(known_list_fn):
+        with open(known_list_fn,'r') as f:
+            known_files = f.readlines()
+        known_files = [file.strip() for file in known_files]
+    else:
+        known_files = []
+        
+    with open(known_list_fn,'a') as f:
+            
+        for sdr in sub_dirs:
+            src_dir_start = os.path.join(source_dir_root,dr,sdr)
+            dst_dir_start = os.path.join(dest_dir_root,dr,sdr)
+            prev_str_len = 0
+            CR.copy_dirs(src_dir_start,dst_dir_start,files_list,known_files,f,prev_str_len)
+        
+        
+        
+        
+        
+        
+        # if not os.path.isdir(dst_dir):
+        #     os.makedirs(dst_dir)
+            
+        # files = 
+    
+    
+    
+
+
---
+++ copy_replace_files_fns.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  7 11:07:57 2022
+
+@author: jmatt
+"""
+
+
+import os
+from shutil import move
+
+
+
+def copy_dirs(src_dir,dst_dir,files_list,known_files,f,prev_str_len):
+    items = os.listdir(src_dir)
+    source_dirs = [item for item in items if os.path.isdir(os.path.join(src_dir,item))]
+    source_files = [item for item in items if os.path.isfile(os.path.join(src_dir,item))]
+    new_files = [item for item in source_files if os.path.join(src_dir,item) not in known_files]
+    
+    for file in new_files:
+        
+        src_file = os.path.join(src_dir,file)
+        string = '{}: {}'.format(str(len(files_list)).zfill(4),src_file)
+        print('\r{}{}'.format(string,' '*(prev_str_len-len(string))),end="")
+        prev_str_len = len(string)
+        dst_file = os.path.join(dst_dir,file)
+        move(src_file,dst_file)
+        with open(src_file,'w') as tmp_file:
+            tmp_file.write('')
+        files_list.append(src_file)
+        f.write('{}\n'.format(src_file))
+    
+    breakhere=1
+        
+    while len(source_dirs)>0:
+        dr = source_dirs.pop()
+        src_dir_new = os.path.join(src_dir,dr)
+        dst_dir_new = os.path.join(dst_dir,dr)
+        
+        if not os.path.isdir(dst_dir_new):
+            os.makedirs(dst_dir_new)
+        
+        copy_dirs(src_dir_new,dst_dir_new,files_list,known_files,f,prev_str_len)
